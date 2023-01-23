4b4e4ee1ddc648c9d52130c5b16a932d9a722e8d
Vedat Can Keklik <vcankeklik@gmail.com>
2023-01-23 11:00:18 +0300

    feat: Entry point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
 fn main() {
-    println!("Hello, world!");
+    println!("sumer v0");
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() > 2 {
+        println!("Too many args. Usage: sumer [script]");
+        std::process::exit(64);
+    }
+    let filename_arg = args.get(1);
+    match filename_arg {
+        Some(filename) => run_file(filename),
+        None => println!("REPL mode")
+    }
+}
+
+fn run_file(filename: &String) {
+    println!("filename: {filename}");
 }
