8f9f441604366a9a9757b16555128f6588a157bb
Vedat Can Keklik <vcankeklik@gmail.com>
2023-01-26 09:28:20 +0300

    feat: Added minimal REPL mode

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+use std::io::{stdin,stdout,Write}; 
 fn main() {
     println!("sumer v0");
     let args: Vec<_> = std::env::args().collect();
@@ -9,10 +9,29 @@ fn main() {
     let filename_arg = args.get(1);
     match filename_arg {
         Some(filename) => run_file(filename),
-        None => println!("REPL mode")
+        None => run_repl()
     }
 }
 
 fn run_file(filename: &String) {
     println!("filename: {filename}");
+}
+
+fn run_repl() {
+    println!("REPL mode");
+    let mut line=String::new();
+    loop {
+        print!("> ");
+        let _ = stdout().flush();
+        stdin().read_line(&mut line).expect("Invalid input");
+        if line.is_empty() || line.trim() == "exit" {
+            break;
+        }
+        run(&mut line);
+        line.clear();
+    }
+}
+
+fn run(source: &String) {
+    println!("{source}");
 }
