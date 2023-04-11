de818de78de6062d6c3e793dc8e42d872c9bdb7f
Vedat Can Keklik <vcankeklik@gmail.com>
2023-04-11 14:13:55 +0300

    feat: Enhanced type space

--- src/lang/parsing/visitors/interpreter.rs
+++ src/lang/parsing/visitors/interpreter.rs
@@ -1,5 +1,3 @@
-use std::process::exit;
-use crate::lang::parsing::error::runtime_err;
 use crate::lang::parsing::expr::{Expr, Visitor};
 use crate::lang::parsing::token::LiteralValue::{Num, Str, Bool, Nil};
 use crate::lang::parsing::token::TokenType::*;
@@ -16,6 +14,8 @@ pub enum RV {
     Str(String),
     Num(f32),
     Bool(bool),
+    Undefined,
+    NaN,
     Nil
 }
 
@@ -40,10 +40,45 @@ impl Visitor<RV> for Interpreter {
                     (RV::Num(l), EqualEqual, RV::Num(r)) => RV::Bool(l == r),
                     //
                     (RV::Str(l), Plus, RV::Str(r)) => RV::Str(l + &r),
-                    (RV::Str(l), Plus, RV::Num(r)) => RV::Str(l + &r.to_string()),
-                    (RV::Num(l), Plus, RV::Str(r)) => RV::Str(l.to_string() + &r),
+                    (RV::Str(l), Less, RV::Str(r)) => RV::Bool(l < r),
+                    (RV::Str(l), LessEqual, RV::Str(r)) => RV::Bool(l <= r),
+                    (RV::Str(l), Greater, RV::Str(r)) => RV::Bool(l > r),
+                    (RV::Str(l), GreaterEqual, RV::Str(r)) => RV::Bool(l >= r),
+                    (RV::Str(l), BangEqual, RV::Str(r)) => RV::Bool(l != r),
+                    (RV::Str(l), EqualEqual, RV::Str(r)) => RV::Bool(l == r),
                     //
-                    (_, _, _) => panic!("Invalid operation")
+                    (RV::Str(str), Plus, RV::Num(num)) => RV::Str(str + &num.to_string()),
+                    (RV::Num(num), Plus, RV::Str(str)) => RV::Str(num.to_string() + &str),
+                    //
+                    (RV::Str(str), Plus, RV::Bool(bool)) |
+                    (RV::Bool(bool), Plus, RV::Str(str)) => RV::Str(str + if bool { "true" } else {"false"}),
+                    //
+                    (RV::Bool(l), BangEqual, RV::Bool(r)) => RV::Bool(l != r),
+                    (RV::Bool(l), EqualEqual, RV::Bool(r)) => RV::Bool(l == r),
+                    //
+                    (RV::Nil, EqualEqual, RV::Nil) => RV::Bool(true),
+                    (RV::Nil, BangEqual, RV::Nil) => RV::Bool(false),
+                    //
+                    (_, EqualEqual, RV::Nil) |
+                    (RV::Nil, EqualEqual, _) => RV::Bool(false),
+                    //
+                    (RV::NaN, Plus, _) | (_, Plus, RV::NaN) |
+                    (RV::NaN, Minus, _) | (_, Minus, RV::NaN) |
+                    (RV::NaN, Star, _) | (_, Star, RV::NaN) |
+                    (RV::NaN, Slash, _) | (_, Slash, RV::NaN) => RV::NaN,
+                    //
+                    (_, Less, _) |
+                    (_, LessEqual, _) |
+                    (_, Greater, _) |
+                    (_, GreaterEqual, _) |
+                    (_, EqualEqual, _) |
+                    (_, BangEqual, _) => RV::Bool(true),
+                    //
+                    (_, Minus, _) |
+                    (_, Star, _) |
+                    (_, Slash, _) => RV::NaN,
+                    //
+                    (_, _, _) => RV::Undefined
                 }
             }
             Expr::Literal(Str(value)) => RV::Str(value.clone()),
@@ -56,10 +91,9 @@ impl Visitor<RV> for Interpreter {
                     let val = self.visit_expr(expr);
                     match val {
                         RV::Num(value) => RV::Num(-value),
-                        _ => {
-                            runtime_err(&format!("Non-numeric {:?} cannot be negated", val), tok.line);
-                            exit(1);
-                        }
+                        RV::Bool(true) => RV::Num(-1.0),
+                        RV::Bool(false) => RV::Num(0.0),
+                        _ => RV::NaN,
                     }
                 }
                 else {
@@ -67,7 +101,10 @@ impl Visitor<RV> for Interpreter {
                         RV::Num(value) => RV::Bool(value == 0.0 || value.is_nan()),
                         RV::Str(value) => RV::Bool(value.is_empty()),
                         RV::Bool(value) => RV::Bool(!value),
-                        RV::Nil => RV::Bool(true),
+                        RV::Nil |
+                        RV::Undefined |
+                        RV::NaN
+                        => RV::Bool(true),
                     }
                 }
             },
