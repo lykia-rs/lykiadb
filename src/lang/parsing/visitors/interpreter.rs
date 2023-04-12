310b104d09ef199e3d23ff03f5c109983b3569df
Vedat Can Keklik <vcankeklik@gmail.com>
2023-04-12 11:37:03 +0300

    feat: Refactored type coercion

--- src/lang/parsing/visitors/interpreter.rs
+++ src/lang/parsing/visitors/interpreter.rs
@@ -21,7 +21,7 @@ pub enum RV {
 }
 
 macro_rules! bool2num {
-    ($val: ident) => {
+    ($val: expr) => {
         if $val { 1.0 } else { 0.0 }
     }
 }
@@ -34,7 +34,28 @@ impl Visitor<RV> for Interpreter {
                 let right_eval = self.visit_expr(right);
                 let tok_type = tok.tok_type.clone();
 
-                match (left_eval, tok_type, right_eval) {
+                let (left_coerced, right_coerced) = match (&left_eval, &tok_type, &right_eval) {
+                    (RV::Num(n), _, RV::Bool(bool)) => (RV::Num(*n), RV::Num(bool2num!(*bool))),
+                    (RV::Bool(bool), _, RV::Num(n)) => (RV::Num(bool2num!(*bool)), RV::Num(*n)),
+                    (RV::Bool(l), Plus, RV::Bool(r)) |
+                    (RV::Bool(l), Minus, RV::Bool(r)) |
+                    (RV::Bool(l), Star, RV::Bool(r)) |
+                    (RV::Bool(l), Slash, RV::Bool(r)) => (RV::Num(bool2num!(*l)), RV::Num(bool2num!(*r))),
+                    (_, _, _) => (left_eval, right_eval)
+                };
+
+                match (left_coerced, tok_type, right_coerced) {
+                    (RV::Nil, EqualEqual, RV::Nil) => RV::Bool(true),
+                    (RV::Nil, BangEqual, RV::Nil) => RV::Bool(false),
+                    //
+                    (_, EqualEqual, RV::Nil) |
+                    (RV::Nil, EqualEqual, _) => RV::Bool(false),
+                    //
+                    (RV::NaN, Plus, _) | (_, Plus, RV::NaN) |
+                    (RV::NaN, Minus, _) | (_, Minus, RV::NaN) |
+                    (RV::NaN, Star, _) | (_, Star, RV::NaN) |
+                    (RV::NaN, Slash, _) | (_, Slash, RV::NaN) => RV::NaN,
+                    //
                     (RV::Num(l), Plus, RV::Num(r)) => RV::Num(l + r),
                     (RV::Num(l), Minus, RV::Num(r)) => RV::Num(l - r),
                     (RV::Num(l), Star, RV::Num(r)) => RV::Num(l * r),
@@ -54,10 +75,6 @@ impl Visitor<RV> for Interpreter {
                     (RV::Str(l), BangEqual, RV::Str(r)) => RV::Bool(l != r),
                     (RV::Str(l), EqualEqual, RV::Str(r)) => RV::Bool(l == r),
                     //
-                    (RV::Bool(l), Plus, RV::Bool(r)) => RV::Num(bool2num!(l) + bool2num!(r)),
-                    (RV::Bool(l), Minus, RV::Bool(r)) => RV::Num(bool2num!(l) - bool2num!(r)),
-                    (RV::Bool(l), Star, RV::Bool(r)) => RV::Num(bool2num!(l) * bool2num!(r)),
-                    (RV::Bool(l), Slash, RV::Bool(r)) => RV::Num(bool2num!(l) / bool2num!(r)),
                     (RV::Bool(l), Less, RV::Bool(r)) => RV::Bool(!l & r),
                     (RV::Bool(l), LessEqual, RV::Bool(r)) => RV::Bool(l <= r),
                     (RV::Bool(l), Greater, RV::Bool(r)) => RV::Bool(l & !r),
@@ -70,38 +87,6 @@ impl Visitor<RV> for Interpreter {
                     //
                     (RV::Str(s), Plus, RV::Bool(bool))  => RV::Str(s + &bool.to_string()),
                     (RV::Bool(bool), Plus, RV::Str(s)) => RV::Str(bool.to_string() + &s),
-                    //
-                    (RV::Num(n), Plus, RV::Bool(bool)) |
-                    (RV::Bool(bool), Plus, RV::Num(n)) => RV::Num(n + bool2num!(bool)),
-                    (RV::Num(n), Minus, RV::Bool(bool)) => RV::Num(n - bool2num!(bool)),
-                    (RV::Bool(bool), Minus, RV::Num(n)) => RV::Num(bool2num!(bool) - n),
-                    (RV::Num(n), Star, RV::Bool(bool)) |
-                    (RV::Bool(bool), Star, RV::Num(n)) => RV::Num(n * bool2num!(bool)),
-                    (RV::Num(n), Slash, RV::Bool(bool)) => RV::Num(n / bool2num!(bool)),
-                    (RV::Bool(bool), Slash, RV::Num(n)) => RV::Num(bool2num!(bool) / n),
-                    (RV::Bool(bool), Less, RV::Num(n)) => RV::Bool(bool2num!(bool) < n),
-                    (RV::Bool(bool), LessEqual, RV::Num(n)) => RV::Bool(bool2num!(bool) <= n),
-                    (RV::Bool(bool), Greater, RV::Num(n)) => RV::Bool(bool2num!(bool) > n),
-                    (RV::Bool(bool), GreaterEqual, RV::Num(n)) => RV::Bool(bool2num!(bool) >= n),
-                    (RV::Num(n), Less, RV::Bool(bool)) => RV::Bool(n < bool2num!(bool)),
-                    (RV::Num(n), LessEqual, RV::Bool(bool)) => RV::Bool(n <= bool2num!(bool)),
-                    (RV::Num(n), Greater, RV::Bool(bool)) => RV::Bool(n > bool2num!(bool)),
-                    (RV::Num(n), GreaterEqual, RV::Bool(bool)) => RV::Bool(n >= bool2num!(bool)),
-                    (RV::Bool(bool), BangEqual, RV::Num(n)) |
-                    (RV::Num(n), BangEqual, RV::Bool(bool)) => RV::Bool(bool2num!(bool) != n),
-                    (RV::Bool(bool), EqualEqual, RV::Num(n)) |
-                    (RV::Num(n), EqualEqual, RV::Bool(bool))  => RV::Bool(bool2num!(bool) == n),
-                    //
-                    (RV::Nil, EqualEqual, RV::Nil) => RV::Bool(true),
-                    (RV::Nil, BangEqual, RV::Nil) => RV::Bool(false),
-                    //
-                    (_, EqualEqual, RV::Nil) |
-                    (RV::Nil, EqualEqual, _) => RV::Bool(false),
-                    //
-                    (RV::NaN, Plus, _) | (_, Plus, RV::NaN) |
-                    (RV::NaN, Minus, _) | (_, Minus, RV::NaN) |
-                    (RV::NaN, Star, _) | (_, Star, RV::NaN) |
-                    (RV::NaN, Slash, _) | (_, Slash, RV::NaN) => RV::NaN,
                     //
                     (_, Less, _) |
                     (_, LessEqual, _) |
