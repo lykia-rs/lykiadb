6dfe019d9fbf3d22b7f82d5ce3d054e040844320
Vedat Can Keklik <vcankeklik@gmail.com>
2023-08-31 10:23:04 +0300

    fix: Lint

--- src/lang/execution/primitives.rs
+++ src/lang/execution/primitives.rs
@@ -1,4 +1,4 @@
-use std::fmt::{Debug, Formatter, Write};
+use std::fmt::{Debug, Formatter};
 use std::process::exit;
 use std::rc::Rc;
 use crate::lang::execution::environment::{Environment, Shared};
