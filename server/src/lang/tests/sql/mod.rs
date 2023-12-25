1ed41dec445a6a14f6d415503dc0aa240eb6449f
Vedat Can Keklik <vcankeklik@gmail.com>
2023-12-26 00:50:40 +0300

    fix: More cases to compound

--- server/src/lang/tests/sql/mod.rs
+++ server/src/lang/tests/sql/mod.rs
@@ -1,6 +1,6 @@
+pub mod select_compound;
 pub mod select_from;
 pub mod select_join;
 pub mod select_limit;
 pub mod select_order;
 pub mod select_projection;
-pub mod select_compound;
--- server/src/lang/tests/sql/select_compound.rs
+++ server/src/lang/tests/sql/select_compound.rs
@@ -49,7 +49,174 @@ assert_parsing! {
                                         "type": "All",
                                         "collection": null
                                     }]
-                                },  
+                                },
+                            }],
+                            "limit": null,
+                            "order_by": null
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    single_intersect: {
+        "SELECT * FROM users INTERSECT SELECT * FROM users;" => {
+            "type": "Stmt::Program",
+            "body": [
+                {
+                    "type": "Stmt::Expression",
+                    "expr": {
+                        "type": "Expr::Select",
+                        "value": {
+                            "core": {
+                                "from": {
+                                    "type": "Group",
+                                    "subqueries": [{
+                                        "type": "Collection",
+                                        "alias": null,
+                                        "name": "users",
+                                        "namespace": null,
+                                    }],
+                                },
+                                "projection": [{
+                                    "type": "All",
+                                    "collection": null
+                                }]
+                            },
+                            "compound": [{
+                                "operation": "Intersect",
+                                "core": {
+                                    "from": {
+                                        "type": "Group",
+                                        "subqueries": [{
+                                            "type": "Collection",
+                                            "alias": null,
+                                            "name": "users",
+                                            "namespace": null,
+                                        }],
+                                    },
+                                    "projection": [{
+                                        "type": "All",
+                                        "collection": null
+                                    }]
+                                },
+                            }],
+                            "limit": null,
+                            "order_by": null
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    single_except: {
+        "SELECT * FROM users EXCEPT SELECT * FROM users;" => {
+            "type": "Stmt::Program",
+            "body": [
+                {
+                    "type": "Stmt::Expression",
+                    "expr": {
+                        "type": "Expr::Select",
+                        "value": {
+                            "core": {
+                                "from": {
+                                    "type": "Group",
+                                    "subqueries": [{
+                                        "type": "Collection",
+                                        "alias": null,
+                                        "name": "users",
+                                        "namespace": null,
+                                    }],
+                                },
+                                "projection": [{
+                                    "type": "All",
+                                    "collection": null
+                                }]
+                            },
+                            "compound": [{
+                                "operation": "Except",
+                                "core": {
+                                    "from": {
+                                        "type": "Group",
+                                        "subqueries": [{
+                                            "type": "Collection",
+                                            "alias": null,
+                                            "name": "users",
+                                            "namespace": null,
+                                        }],
+                                    },
+                                    "projection": [{
+                                        "type": "All",
+                                        "collection": null
+                                    }]
+                                },
+                            }],
+                            "limit": null,
+                            "order_by": null
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    union_and_except: {
+        "SELECT * FROM users UNION SELECT * FROM users EXCEPT SELECT * FROM users;" => {
+            "type": "Stmt::Program",
+            "body": [
+                {
+                    "type": "Stmt::Expression",
+                    "expr": {
+                        "type": "Expr::Select",
+                        "value": {
+                            "core": {
+                                "from": {
+                                    "type": "Group",
+                                    "subqueries": [{
+                                        "type": "Collection",
+                                        "alias": null,
+                                        "name": "users",
+                                        "namespace": null,
+                                    }],
+                                },
+                                "projection": [{
+                                    "type": "All",
+                                    "collection": null
+                                }]
+                            },
+                            "compound": [{
+                                "operation": "Union",
+                                "core": {
+                                    "from": {
+                                        "type": "Group",
+                                        "subqueries": [{
+                                            "type": "Collection",
+                                            "alias": null,
+                                            "name": "users",
+                                            "namespace": null,
+                                        }],
+                                    },
+                                    "projection": [{
+                                        "type": "All",
+                                        "collection": null
+                                    }]
+                                },
+                            },{
+                                "operation": "Except",
+                                "core": {
+                                    "from": {
+                                        "type": "Group",
+                                        "subqueries": [{
+                                            "type": "Collection",
+                                            "alias": null,
+                                            "name": "users",
+                                            "namespace": null,
+                                        }],
+                                    },
+                                    "projection": [{
+                                        "type": "All",
+                                        "collection": null
+                                    }]
+                                },
                             }],
                             "limit": null,
                             "order_by": null
