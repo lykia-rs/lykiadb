d852f4b5ce41af9190b073471736e7d318ddb7b5
Vedat Can Keklik <vcankeklik@gmail.com>
2023-12-31 13:38:57 +0300

    feat: More group by tests

--- server/src/lang/tests/sql/select_group_by.rs
+++ server/src/lang/tests/sql/select_group_by.rs
@@ -63,5 +63,163 @@ assert_parsing! {
                   "type": "Stmt::Expression"
             }]
         }
+    },
+    more_complex_0: {
+        "SELECT avg(salary) from employees group by department_id having avg(salary) > 1000;" => {
+            "type": "Stmt::Program",
+            "body": [{
+                  "expr": {
+                    "type": "Expr::Select",
+                    "value": {
+                      "compound": [],
+                      "core": {
+                        "distinct": "ImplicitAll",
+                        "from": {
+                          "subqueries": [
+                            {
+                              "alias": null,
+                              "name": "employees",
+                              "namespace": null,
+                              "type": "Collection"
+                            }
+                          ],
+                          "type": "Group"
+                        },
+                        "projection": [
+                          {
+                            "alias": null,
+                            "expr": {
+                               "callee": {
+                                  "name": "avg",
+                                  "type": "Expr::Variable"
+                              },
+                              "args": [
+                                {
+                                  "name": "salary",
+                                  "type": "Expr::Variable"
+                                }
+                              ],
+                              "type": "Expr::Call"
+                            },
+                            "type": "Expr"
+                          }
+                        ],
+                        "where": null,
+                        "group_by":  [{
+                            "name": "department_id",
+                            "type": "Expr::Variable"
+                        }],
+                        "having": {
+                            "type": "Expr::Binary",
+                            "operation": "Greater",
+                            "left": {
+                                "callee": {
+                                    "name": "avg",
+                                    "type": "Expr::Variable"
+                                },
+                                "args": [
+                                    {
+                                        "name": "salary",
+                                        "type": "Expr::Variable"
+                                    }
+                                ],
+                                "type": "Expr::Call"
+                            },
+                            "right": {
+                                "type": "Expr::Literal",
+                                "value": "Num(1000.0)",
+                                "raw": "1000"
+                            }
+                        }
+                      },
+                      "limit": null,
+                      "order_by": null
+                    }
+                  },
+                  "type": "Stmt::Expression"
+            }]
+        }
+    },
+    more_complex_1: {
+        "SELECT avg(salary) from employees group by department_id, job_id having avg(salary) > 1000;" => {
+            "type": "Stmt::Program",
+            "body": [{
+                  "expr": {
+                    "type": "Expr::Select",
+                    "value": {
+                      "compound": [],
+                      "core": {
+                        "distinct": "ImplicitAll",
+                        "from": {
+                          "subqueries": [
+                            {
+                              "alias": null,
+                              "name": "employees",
+                              "namespace": null,
+                              "type": "Collection"
+                            }
+                          ],
+                          "type": "Group"
+                        },
+                        "projection": [
+                          {
+                            "alias": null,
+                            "expr": {
+                               "callee": {
+                                  "name": "avg",
+                                  "type": "Expr::Variable"
+                              },
+                              "args": [
+                                {
+                                  "name": "salary",
+                                  "type": "Expr::Variable"
+                                }
+                              ],
+                              "type": "Expr::Call"
+                            },
+                            "type": "Expr"
+                          }
+                        ],
+                        "where": null,
+                        "group_by":  [
+                            {
+                                "name": "department_id",
+                                "type": "Expr::Variable"
+                            },
+                            {
+                                "name": "job_id",
+                                "type": "Expr::Variable"
+                            }
+                        ],
+                        "having": {
+                            "type": "Expr::Binary",
+                            "operation": "Greater",
+                            "left": {
+                                "callee": {
+                                    "name": "avg",
+                                    "type": "Expr::Variable"
+                                },
+                                "args": [
+                                    {
+                                        "name": "salary",
+                                        "type": "Expr::Variable"
+                                    }
+                                ],
+                                "type": "Expr::Call"
+                            },
+                            "right": {
+                                "type": "Expr::Literal",
+                                "value": "Num(1000.0)",
+                                "raw": "1000"
+                            }
+                        }
+                      },
+                      "limit": null,
+                      "order_by": null
+                    }
+                  },
+                  "type": "Stmt::Expression"
+            }]
+        }
     }
 }
