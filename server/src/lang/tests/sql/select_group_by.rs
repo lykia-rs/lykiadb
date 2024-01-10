64f65629fc9353d0ac7601c7902781fcf5bb7e48
Vedat Can Keklik <vcankeklik@gmail.com>
2024-01-11 00:45:50 +0300

    fix: Grouping tests

--- server/src/lang/tests/sql/select_group_by.rs
+++ server/src/lang/tests/sql/select_group_by.rs
@@ -11,215 +11,313 @@ use crate::assert_parsing;
 assert_parsing! {
     plain: {
         "SELECT avg(salary) from employees group by department_id;" => {
-            "type": "Stmt::Program",
-            "body": [{
-                  "expr": {
-                    "type": "Expr::Select",
-                    "value": {
-                      "compound": [],
-                      "core": {
-                        "distinct": "ImplicitAll",
-                        "from": {
-                          "subqueries": [
+          "@type": "Stmt::Program",
+          "body": [
+            {
+              "@type": "Stmt::Expression",
+              "expr": {
+                "@type": "Expr::Select",
+                "query": {
+                  "@type": "SqlSelect",
+                  "core": {
+                    "@type": "SqlSelectCore",
+                    "compound": null,
+                    "distinct": {
+                      "@type": "SqlDistinct::ImplicitAll"
+                    },
+                    "from": {
+                      "@type": "SqlCollectionSubquery::Group",
+                      "values": [
+                        {
+                          "@type": "SqlCollectionIdentifier",
+                          "alias": null,
+                          "name": {
+                            "@type": "Identifier",
+                            "dollar": false,
+                            "name": "employees"
+                          },
+                          "namespace": null
+                        }
+                      ]
+                    },
+                    "group_by": [
+                      {
+                        "@type": "Expr::Variable",
+                        "name": {
+                          "@type": "Identifier",
+                          "dollar": false,
+                          "name": "department_id"
+                        }
+                      }
+                    ],
+                    "having": null,
+                    "projection": [
+                      {
+                        "@type": "SqlProjection::Expr",
+                        "alias": null,
+                        "expr": {
+                          "@type": "Expr::Call",
+                          "args": [
                             {
-                              "alias": null,
-                              "name": "employees",
-                              "namespace": null,
-                              "type": "Collection"
+                              "@type": "Expr::Variable",
+                              "name": {
+                                "@type": "Identifier",
+                                "dollar": false,
+                                "name": "salary"
+                              }
                             }
                           ],
-                          "type": "Group"
-                        },
-                        "projection": [
-                          {
-                            "alias": null,
-                            "expr": {
-                               "callee": {
-                                  "name": "avg",
-                                  "type": "Expr::Variable"
-                              },
-                              "args": [
-                                {
-                                  "name": "salary",
-                                  "type": "Expr::Variable"
-                                }
-                              ],
-                              "type": "Expr::Call"
-                            },
-                            "type": "Expr"
+                          "callee": {
+                            "@type": "Expr::Variable",
+                            "name": {
+                              "@type": "Identifier",
+                              "dollar": false,
+                              "name": "avg"
+                            }
                           }
-                        ],
-                        "where": null,
-                        "group_by":  [{
-                            "name": "department_id",
-                            "type": "Expr::Variable"
-                        }],
-                        "having": null
-                      },
-                      "limit": null,
-                      "order_by": null
-                    }
+                        }
+                      }
+                    ],
+                    "where": null
                   },
-                  "type": "Stmt::Expression"
-            }]
+                  "limit": null,
+                  "order_by": null
+                }
+              }
+            }
+          ]
         }
     },
     more_complex_0: {
         "SELECT avg(salary) from employees group by department_id having avg(salary) > 1000;" => {
-            "type": "Stmt::Program",
-            "body": [{
-                  "expr": {
-                    "type": "Expr::Select",
-                    "value": {
-                      "compound": [],
-                      "core": {
-                        "distinct": "ImplicitAll",
-                        "from": {
-                          "subqueries": [
-                            {
-                              "alias": null,
-                              "name": "employees",
-                              "namespace": null,
-                              "type": "Collection"
-                            }
-                          ],
-                          "type": "Group"
-                        },
-                        "projection": [
+          "@type": "Stmt::Program",
+          "body": [
+            {
+              "@type": "Stmt::Expression",
+              "expr": {
+                "@type": "Expr::Select",
+                "query": {
+                  "@type": "SqlSelect",
+                  "core": {
+                    "@type": "SqlSelectCore",
+                    "compound": null,
+                    "distinct": {
+                      "@type": "SqlDistinct::ImplicitAll"
+                    },
+                    "from": {
+                      "@type": "SqlCollectionSubquery::Group",
+                      "values": [
+                        {
+                          "@type": "SqlCollectionIdentifier",
+                          "alias": null,
+                          "name": {
+                            "@type": "Identifier",
+                            "dollar": false,
+                            "name": "employees"
+                          },
+                          "namespace": null
+                        }
+                      ]
+                    },
+                    "group_by": [
+                      {
+                        "@type": "Expr::Variable",
+                        "name": {
+                          "@type": "Identifier",
+                          "dollar": false,
+                          "name": "department_id"
+                        }
+                      }
+                    ],
+                    "having": {
+                      "@type": "Expr::Binary",
+                      "left": {
+                        "@type": "Expr::Call",
+                        "args": [
                           {
-                            "alias": null,
-                            "expr": {
-                               "callee": {
-                                  "name": "avg",
-                                  "type": "Expr::Variable"
-                              },
-                              "args": [
-                                {
-                                  "name": "salary",
-                                  "type": "Expr::Variable"
-                                }
-                              ],
-                              "type": "Expr::Call"
-                            },
-                            "type": "Expr"
+                            "@type": "Expr::Variable",
+                            "name": {
+                              "@type": "Identifier",
+                              "dollar": false,
+                              "name": "salary"
+                            }
                           }
                         ],
-                        "where": null,
-                        "group_by":  [{
-                            "name": "department_id",
-                            "type": "Expr::Variable"
-                        }],
-                        "having": {
-                            "type": "Expr::Binary",
-                            "operation": "Greater",
-                            "left": {
-                                "callee": {
-                                    "name": "avg",
-                                    "type": "Expr::Variable"
-                                },
-                                "args": [
-                                    {
-                                        "name": "salary",
-                                        "type": "Expr::Variable"
-                                    }
-                                ],
-                                "type": "Expr::Call"
-                            },
-                            "right": {
-                                "type": "Expr::Literal",
-                                "value": "Num(1000.0)",
-                                "raw": "1000"
-                            }
+                        "callee": {
+                          "@type": "Expr::Variable",
+                          "name": {
+                            "@type": "Identifier",
+                            "dollar": false,
+                            "name": "avg"
+                          }
                         }
                       },
-                      "limit": null,
-                      "order_by": null
-                    }
+                      "operation": {
+                        "@type": "Greater"
+                      },
+                      "right": {
+                        "@type": "Expr::Literal",
+                        "raw": "1000",
+                        "value": {
+                          "Num": 1000.0
+                        }
+                      }
+                    },
+                    "projection": [
+                      {
+                        "@type": "SqlProjection::Expr",
+                        "alias": null,
+                        "expr": {
+                          "@type": "Expr::Call",
+                          "args": [
+                            {
+                              "@type": "Expr::Variable",
+                              "name": {
+                                "@type": "Identifier",
+                                "dollar": false,
+                                "name": "salary"
+                              }
+                            }
+                          ],
+                          "callee": {
+                            "@type": "Expr::Variable",
+                            "name": {
+                              "@type": "Identifier",
+                              "dollar": false,
+                              "name": "avg"
+                            }
+                          }
+                        }
+                      }
+                    ],
+                    "where": null
                   },
-                  "type": "Stmt::Expression"
-            }]
+                  "limit": null,
+                  "order_by": null
+                }
+              }
+            }
+          ]
         }
     },
     more_complex_1: {
         "SELECT avg(salary) from employees group by department_id, job_id having avg(salary) > 1000;" => {
-            "type": "Stmt::Program",
-            "body": [{
-                  "expr": {
-                    "type": "Expr::Select",
-                    "value": {
-                      "compound": [],
-                      "core": {
-                        "distinct": "ImplicitAll",
-                        "from": {
-                          "subqueries": [
-                            {
-                              "alias": null,
-                              "name": "employees",
-                              "namespace": null,
-                              "type": "Collection"
-                            }
-                          ],
-                          "type": "Group"
-                        },
-                        "projection": [
-                          {
-                            "alias": null,
-                            "expr": {
-                               "callee": {
-                                  "name": "avg",
-                                  "type": "Expr::Variable"
-                              },
-                              "args": [
-                                {
-                                  "name": "salary",
-                                  "type": "Expr::Variable"
-                                }
-                              ],
-                              "type": "Expr::Call"
-                            },
-                            "type": "Expr"
-                          }
-                        ],
-                        "where": null,
-                        "group_by":  [
-                            {
-                                "name": "department_id",
-                                "type": "Expr::Variable"
-                            },
-                            {
-                                "name": "job_id",
-                                "type": "Expr::Variable"
-                            }
-                        ],
-                        "having": {
-                            "type": "Expr::Binary",
-                            "operation": "Greater",
-                            "left": {
-                                "callee": {
-                                    "name": "avg",
-                                    "type": "Expr::Variable"
-                                },
-                                "args": [
-                                    {
-                                        "name": "salary",
-                                        "type": "Expr::Variable"
-                                    }
-                                ],
-                                "type": "Expr::Call"
-                            },
-                            "right": {
-                                "type": "Expr::Literal",
-                                "value": "Num(1000.0)",
-                                "raw": "1000"
-                            }
+          "@type": "Stmt::Program",
+          "body": [
+            {
+              "@type": "Stmt::Expression",
+              "expr": {
+                "@type": "Expr::Select",
+                "query": {
+                  "@type": "SqlSelect",
+                  "core": {
+                    "@type": "SqlSelectCore",
+                    "compound": null,
+                    "distinct": {
+                      "@type": "SqlDistinct::ImplicitAll"
+                    },
+                    "from": {
+                      "@type": "SqlCollectionSubquery::Group",
+                      "values": [
+                        {
+                          "@type": "SqlCollectionIdentifier",
+                          "alias": null,
+                          "name": {
+                            "@type": "Identifier",
+                            "dollar": false,
+                            "name": "employees"
+                          },
+                          "namespace": null
+                        }
+                      ]
+                    },
+                    "group_by": [
+                      {
+                        "@type": "Expr::Variable",
+                        "name": {
+                          "@type": "Identifier",
+                          "dollar": false,
+                          "name": "department_id"
                         }
                       },
-                      "limit": null,
-                      "order_by": null
-                    }
+                      {
+                        "@type": "Expr::Variable",
+                        "name": {
+                          "@type": "Identifier",
+                          "dollar": false,
+                          "name": "job_id"
+                        }
+                      }
+                    ],
+                    "having": {
+                      "@type": "Expr::Binary",
+                      "left": {
+                        "@type": "Expr::Call",
+                        "args": [
+                          {
+                            "@type": "Expr::Variable",
+                            "name": {
+                              "@type": "Identifier",
+                              "dollar": false,
+                              "name": "salary"
+                            }
+                          }
+                        ],
+                        "callee": {
+                          "@type": "Expr::Variable",
+                          "name": {
+                            "@type": "Identifier",
+                            "dollar": false,
+                            "name": "avg"
+                          }
+                        }
+                      },
+                      "operation": {
+                        "@type": "Greater"
+                      },
+                      "right": {
+                        "@type": "Expr::Literal",
+                        "raw": "1000",
+                        "value": {
+                          "Num": 1000.0
+                        }
+                      }
+                    },
+                    "projection": [
+                      {
+                        "@type": "SqlProjection::Expr",
+                        "alias": null,
+                        "expr": {
+                          "@type": "Expr::Call",
+                          "args": [
+                            {
+                              "@type": "Expr::Variable",
+                              "name": {
+                                "@type": "Identifier",
+                                "dollar": false,
+                                "name": "salary"
+                              }
+                            }
+                          ],
+                          "callee": {
+                            "@type": "Expr::Variable",
+                            "name": {
+                              "@type": "Identifier",
+                              "dollar": false,
+                              "name": "avg"
+                            }
+                          }
+                        }
+                      }
+                    ],
+                    "where": null
                   },
-                  "type": "Stmt::Expression"
-            }]
+                  "limit": null,
+                  "order_by": null
+                }
+              }
+            }
+          ]
         }
     }
 }
