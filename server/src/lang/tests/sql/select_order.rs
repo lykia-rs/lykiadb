e17aa2cb39ff09fa93c6cded1d5cb5d114712d18
Vedat Can Keklik <vcankeklik@gmail.com>
2024-01-11 01:17:28 +0300

    fix: Order tests

--- server/src/lang/tests/sql/select_order.rs
+++ server/src/lang/tests/sql/select_order.rs
@@ -10,229 +10,350 @@ use crate::assert_parsing;
 #[cfg(test)]
 assert_parsing! {
     no_order: {
-        "SELECT * from users;" => ""
+        "SELECT * from users;" => {
+          "@type": "Stmt::Program",
+          "body": [
+            {
+              "@type": "Stmt::Expression",
+              "expr": {
+                "@type": "Expr::Select",
+                "query": {
+                  "@type": "SqlSelect",
+                  "core": {
+                    "@type": "SqlSelectCore",
+                    "compound": null,
+                    "distinct": {
+                      "@type": "SqlDistinct::ImplicitAll"
+                    },
+                    "from": {
+                      "@type": "SqlCollectionSubquery::Group",
+                      "values": [
+                        {
+                          "@type": "SqlCollectionIdentifier",
+                          "alias": null,
+                          "name": {
+                            "@type": "Identifier",
+                            "dollar": false,
+                            "name": "users"
+                          },
+                          "namespace": null
+                        }
+                      ]
+                    },
+                    "group_by": null,
+                    "having": null,
+                    "projection": [
+                      {
+                        "@type": "SqlProjection::All",
+                        "collection": null
+                      }
+                    ],
+                    "where": null
+                  },
+                  "limit": null,
+                  "order_by": null
+                }
+              }
+            }
+          ]
+        }
     },
     single_order: {
-        "SELECT * from users order by id desc;" =>         {
-            "@type": "Stmt::Program",
-            "body": [
-              {
-                "@type": "Stmt::Expression",
-                "expr": {
-                  "@type": "Expr::Select",
-                  "query": {
-                    "@type": "SqlSelect",
-                    "compound": [],
-                    "core": {
-                      "@type": "SqlSelectCore",
-                      "distinct": {
-                        "@type": "SqlDistinct::ImplicitAll"
-                      },
-                      "from": {
-                        "@type": "SqlCollectionSubquery::Group",
-                        "values": [
-                          {
-                            "@type": "SqlCollectionIdentifier",
-                            "alias": null,
-                            "name": {
-                              "@type": "Identifier",
-                              "dollar": false,
-                              "name": "users"
-                            },
-                            "namespace": null
-                          }
-                        ]
-                      },
-                      "group_by": null,
-                      "having": null,
-                      "projection": [
-                        {
-                          "@type": "SqlProjection::All",
-                          "collection": null
-                        }
-                      ],
-                      "where": null
+        "SELECT * from users order by id desc;" => {
+          "@type": "Stmt::Program",
+          "body": [
+            {
+              "@type": "Stmt::Expression",
+              "expr": {
+                "@type": "Expr::Select",
+                "query": {
+                  "@type": "SqlSelect",
+                  "core": {
+                    "@type": "SqlSelectCore",
+                    "compound": null,
+                    "distinct": {
+                      "@type": "SqlDistinct::ImplicitAll"
                     },
-                    "limit": null,
-                    "order_by": [
-                      {
-                        "@type": "SqlOrderByClause",
-                        "expr": {
-                          "@type": "Expr::Variable",
+                    "from": {
+                      "@type": "SqlCollectionSubquery::Group",
+                      "values": [
+                        {
+                          "@type": "SqlCollectionIdentifier",
+                          "alias": null,
                           "name": {
                             "@type": "Identifier",
                             "dollar": false,
-                            "name": "id"
-                          }
-                        },
-                        "ordering": {
-                          "@type": "SqlOrdering::Desc"
+                            "name": "users"
+                          },
+                          "namespace": null
                         }
+                      ]
+                    },
+                    "group_by": null,
+                    "having": null,
+                    "projection": [
+                      {
+                        "@type": "SqlProjection::All",
+                        "collection": null
                       }
-                    ]
-                  }
+                    ],
+                    "where": null
+                  },
+                  "limit": null,
+                  "order_by": [
+                    {
+                      "@type": "SqlOrderByClause",
+                      "expr": {
+                        "@type": "Expr::Variable",
+                        "name": {
+                          "@type": "Identifier",
+                          "dollar": false,
+                          "name": "id"
+                        }
+                      },
+                      "ordering": {
+                        "@type": "SqlOrdering::Desc"
+                      }
+                    }
+                  ]
                 }
               }
-            ]
-          }
+            }
+          ]
+        }
     },
     multiple_order_0: {
-        "SELECT * from users order by id, name;" =>  ""
+        "SELECT * from users order by id, name;" => {
+          "@type": "Stmt::Program",
+          "body": [
+            {
+              "@type": "Stmt::Expression",
+              "expr": {
+                "@type": "Expr::Select",
+                "query": {
+                  "@type": "SqlSelect",
+                  "core": {
+                    "@type": "SqlSelectCore",
+                    "compound": null,
+                    "distinct": {
+                      "@type": "SqlDistinct::ImplicitAll"
+                    },
+                    "from": {
+                      "@type": "SqlCollectionSubquery::Group",
+                      "values": [
+                        {
+                          "@type": "SqlCollectionIdentifier",
+                          "alias": null,
+                          "name": {
+                            "@type": "Identifier",
+                            "dollar": false,
+                            "name": "users"
+                          },
+                          "namespace": null
+                        }
+                      ]
+                    },
+                    "group_by": null,
+                    "having": null,
+                    "projection": [
+                      {
+                        "@type": "SqlProjection::All",
+                        "collection": null
+                      }
+                    ],
+                    "where": null
+                  },
+                  "limit": null,
+                  "order_by": [
+                    {
+                      "@type": "SqlOrderByClause",
+                      "expr": {
+                        "@type": "Expr::Variable",
+                        "name": {
+                          "@type": "Identifier",
+                          "dollar": false,
+                          "name": "id"
+                        }
+                      },
+                      "ordering": {
+                        "@type": "SqlOrdering::Asc"
+                      }
+                    },
+                    {
+                      "@type": "SqlOrderByClause",
+                      "expr": {
+                        "@type": "Expr::Variable",
+                        "name": {
+                          "@type": "Identifier",
+                          "dollar": false,
+                          "name": "name"
+                        }
+                      },
+                      "ordering": {
+                        "@type": "SqlOrdering::Asc"
+                      }
+                    }
+                  ]
+                }
+              }
+            }
+          ]
+        }
     },
     multiple_order_1: {
-        "SELECT * from users order by id asc, name;" => {
-            "@type": "Stmt::Program",
-            "body": [
-              {
-                "@type": "Stmt::Expression",
-                "expr": {
-                  "@type": "Expr::Select",
-                  "query": {
-                    "@type": "SqlSelect",
-                    "compound": [],
-                    "core": {
-                      "@type": "SqlSelectCore",
-                      "distinct": {
-                        "@type": "SqlDistinct::ImplicitAll"
-                      },
-                      "from": {
-                        "@type": "SqlCollectionSubquery::Group",
-                        "values": [
-                          {
-                            "@type": "SqlCollectionIdentifier",
-                            "alias": null,
-                            "name": {
-                              "@type": "Identifier",
-                              "dollar": false,
-                              "name": "users"
-                            },
-                            "namespace": null
-                          }
-                        ]
-                      },
-                      "group_by": null,
-                      "having": null,
-                      "projection": [
-                        {
-                          "@type": "SqlProjection::All",
-                          "collection": null
-                        }
-                      ],
-                      "where": null
+        "SELECT * from users order by id asc, name;" =>         {
+          "@type": "Stmt::Program",
+          "body": [
+            {
+              "@type": "Stmt::Expression",
+              "expr": {
+                "@type": "Expr::Select",
+                "query": {
+                  "@type": "SqlSelect",
+                  "core": {
+                    "@type": "SqlSelectCore",
+                    "compound": null,
+                    "distinct": {
+                      "@type": "SqlDistinct::ImplicitAll"
                     },
-                    "limit": null,
-                    "order_by": [
-                      {
-                        "@type": "SqlOrderByClause",
-                        "expr": {
-                          "@type": "Expr::Variable",
+                    "from": {
+                      "@type": "SqlCollectionSubquery::Group",
+                      "values": [
+                        {
+                          "@type": "SqlCollectionIdentifier",
+                          "alias": null,
                           "name": {
                             "@type": "Identifier",
                             "dollar": false,
-                            "name": "id"
-                          }
-                        },
-                        "ordering": {
-                          "@type": "SqlOrdering::Asc"
+                            "name": "users"
+                          },
+                          "namespace": null
+                        }
+                      ]
+                    },
+                    "group_by": null,
+                    "having": null,
+                    "projection": [
+                      {
+                        "@type": "SqlProjection::All",
+                        "collection": null
+                      }
+                    ],
+                    "where": null
+                  },
+                  "limit": null,
+                  "order_by": [
+                    {
+                      "@type": "SqlOrderByClause",
+                      "expr": {
+                        "@type": "Expr::Variable",
+                        "name": {
+                          "@type": "Identifier",
+                          "dollar": false,
+                          "name": "id"
                         }
                       },
-                      {
-                        "@type": "SqlOrderByClause",
-                        "expr": {
-                          "@type": "Expr::Variable",
-                          "name": {
-                            "@type": "Identifier",
-                            "dollar": false,
-                            "name": "name"
-                          }
-                        },
-                        "ordering": {
-                          "@type": "SqlOrdering::Asc"
-                        }
+                      "ordering": {
+                        "@type": "SqlOrdering::Asc"
                       }
-                    ]
-                  }
+                    },
+                    {
+                      "@type": "SqlOrderByClause",
+                      "expr": {
+                        "@type": "Expr::Variable",
+                        "name": {
+                          "@type": "Identifier",
+                          "dollar": false,
+                          "name": "name"
+                        }
+                      },
+                      "ordering": {
+                        "@type": "SqlOrdering::Asc"
+                      }
+                    }
+                  ]
                 }
               }
-            ]
-          }
+            }
+          ]
+        }
     },
     multiple_order_2: {
-        "SELECT * from users order by id, name desc;" =>         {
-            "@type": "Stmt::Program",
-            "body": [
-              {
-                "@type": "Stmt::Expression",
-                "expr": {
-                  "@type": "Expr::Select",
-                  "query": {
-                    "@type": "SqlSelect",
-                    "compound": [],
-                    "core": {
-                      "@type": "SqlSelectCore",
-                      "distinct": {
-                        "@type": "SqlDistinct::ImplicitAll"
-                      },
-                      "from": {
-                        "@type": "SqlCollectionSubquery::Group",
-                        "values": [
-                          {
-                            "@type": "SqlCollectionIdentifier",
-                            "alias": null,
-                            "name": {
-                              "@type": "Identifier",
-                              "dollar": false,
-                              "name": "users"
-                            },
-                            "namespace": null
-                          }
-                        ]
-                      },
-                      "group_by": null,
-                      "having": null,
-                      "projection": [
-                        {
-                          "@type": "SqlProjection::All",
-                          "collection": null
-                        }
-                      ],
-                      "where": null
+        "SELECT * from users order by id, name desc;" => {
+          "@type": "Stmt::Program",
+          "body": [
+            {
+              "@type": "Stmt::Expression",
+              "expr": {
+                "@type": "Expr::Select",
+                "query": {
+                  "@type": "SqlSelect",
+                  "core": {
+                    "@type": "SqlSelectCore",
+                    "compound": null,
+                    "distinct": {
+                      "@type": "SqlDistinct::ImplicitAll"
                     },
-                    "limit": null,
-                    "order_by": [
-                      {
-                        "@type": "SqlOrderByClause",
-                        "expr": {
-                          "@type": "Expr::Variable",
+                    "from": {
+                      "@type": "SqlCollectionSubquery::Group",
+                      "values": [
+                        {
+                          "@type": "SqlCollectionIdentifier",
+                          "alias": null,
                           "name": {
                             "@type": "Identifier",
                             "dollar": false,
-                            "name": "id"
-                          }
-                        },
-                        "ordering": {
-                          "@type": "SqlOrdering::Asc"
+                            "name": "users"
+                          },
+                          "namespace": null
+                        }
+                      ]
+                    },
+                    "group_by": null,
+                    "having": null,
+                    "projection": [
+                      {
+                        "@type": "SqlProjection::All",
+                        "collection": null
+                      }
+                    ],
+                    "where": null
+                  },
+                  "limit": null,
+                  "order_by": [
+                    {
+                      "@type": "SqlOrderByClause",
+                      "expr": {
+                        "@type": "Expr::Variable",
+                        "name": {
+                          "@type": "Identifier",
+                          "dollar": false,
+                          "name": "id"
                         }
                       },
-                      {
-                        "@type": "SqlOrderByClause",
-                        "expr": {
-                          "@type": "Expr::Variable",
-                          "name": {
-                            "@type": "Identifier",
-                            "dollar": false,
-                            "name": "name"
-                          }
-                        },
-                        "ordering": {
-                          "@type": "SqlOrdering::Desc"
-                        }
+                      "ordering": {
+                        "@type": "SqlOrdering::Asc"
                       }
-                    ]
-                  }
+                    },
+                    {
+                      "@type": "SqlOrderByClause",
+                      "expr": {
+                        "@type": "Expr::Variable",
+                        "name": {
+                          "@type": "Identifier",
+                          "dollar": false,
+                          "name": "name"
+                        }
+                      },
+                      "ordering": {
+                        "@type": "SqlOrdering::Desc"
+                      }
+                    }
+                  ]
                 }
               }
-            ]
-          }
+            }
+          ]
+        }
     }
 }
