b2e2b143317e5b5cf9be4067a6543569c254d9a9
Vedat Can Keklik <vcankeklik@gmail.com>
2024-09-23 10:17:24 +0300

    fix: Test formatting

--- lykiadb-server/tests/planner/join.rs
+++ lykiadb-server/tests/planner/join.rs
@@ -3,8 +3,10 @@ use lykiadb_server::{assert_plan, plan::planner::test_helpers::expect_plan};
 
 assert_plan! {
     three_way_simple: {
-        "SELECT * FROM books b INNER JOIN categories c ON b.category_id = c.id 
-        INNER JOIN publishers AS p ON b.publisher_id = p.id where p.name = 'Springer';" => 
+        "SELECT * FROM books b
+            INNER JOIN categories c ON b.category_id = c.id
+            INNER JOIN publishers AS p ON b.publisher_id = p.id
+            WHERE p.name = 'Springer';" =>
 
 "- filter (p.name IsEqual Str(\"Springer\")):
   - join [Inner, (b.publisher_id IsEqual p.id)]:
