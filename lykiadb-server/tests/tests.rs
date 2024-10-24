e32053cab2773b221ac826dd336d641a303c8dd2
Vedat Can Keklik <vcankeklik@gmail.com>
2024-10-24 13:42:38 +0300

    fix: New test structure for planner

--- lykiadb-server/tests/tests.rs
+++ lykiadb-server/tests/tests.rs
@@ -1,4 +1,8 @@
 #![recursion_limit = "192"]
+use util::run_test;
+use test_each_file::test_each_file;
 
 mod runtime;
-mod planner;
+mod util;
+
+test_each_file! { in "lykiadb-server/tests/planner" => run_test }
