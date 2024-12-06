eca370e7a4d4c108aec2842f854cf0b8c8ca8f70
Vedat Can Keklik <vcankeklik@gmail.com>
2024-12-06 16:58:49 +0300

    fix: Better test harness

--- lykiadb-server/tests/tests.rs
+++ lykiadb-server/tests/tests.rs
@@ -1,14 +1,13 @@
-mod util;
 mod planner {
+    use lykiadb_server::engine::interpreter::test_helpers::InterpreterTester;
     use test_each_file::test_each_file;
-    use crate::util::run_test_file;
 
-    test_each_file! { in "lykiadb-server/tests/planner" => run_test_file }
+    test_each_file! { in "lykiadb-server/tests/planner" => InterpreterTester::test_file }
 }
 
 mod interpreter {
+    use lykiadb_server::engine::interpreter::test_helpers::InterpreterTester;
     use test_each_file::test_each_file;
-    use crate::util::run_test_file;
 
-    test_each_file! { in "lykiadb-server/tests/interpreter" => run_test_file }
+    test_each_file! { in "lykiadb-server/tests/interpreter" => InterpreterTester::test_file }
 }
