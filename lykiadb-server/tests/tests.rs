08c93363f4cde7edc96a6d7f375f95d52995de5b
Vedat Can Keklik <vcankeklik@gmail.com>
2024-12-06 17:13:34 +0300

    fix: Moved test helpers to runtime

--- lykiadb-server/tests/tests.rs
+++ lykiadb-server/tests/tests.rs
@@ -1,13 +1,13 @@
 mod planner {
-    use lykiadb_server::engine::interpreter::test_helpers::InterpreterTester;
+    use lykiadb_server::engine::test_helpers::RuntimeTester;
     use test_each_file::test_each_file;
 
-    test_each_file! { in "lykiadb-server/tests/planner" => InterpreterTester::test_file }
+    test_each_file! { in "lykiadb-server/tests/planner" => RuntimeTester::test_file }
 }
 
 mod interpreter {
-    use lykiadb_server::engine::interpreter::test_helpers::InterpreterTester;
+    use lykiadb_server::engine::test_helpers::RuntimeTester;
     use test_each_file::test_each_file;
 
-    test_each_file! { in "lykiadb-server/tests/interpreter" => InterpreterTester::test_file }
+    test_each_file! { in "lykiadb-server/tests/interpreter" => RuntimeTester::test_file }
 }
