0a72f532017d571d8bf576dd0f674dbab6f38f1e
V. Can Keklik <vcankeklik@gmail.com>
2026-05-25 15:09:55 +0300

    chore: Main definitions

--- lykiadb-server/src/database/mod.rs
+++ lykiadb-server/src/database/mod.rs
@@ -1,16 +1,25 @@
-use crate::{database::storage::{Key, StorageIteratorItem}, execution::error::ExecutionError, value::RV};
-use crate::storage::StorageEngine;
+use crate::{database::{error::DatabaseError, storage::{Key, StorageIteratorItem}}, value::RV};
 
 pub mod error;
 pub mod storage;
+pub mod simple;
 
-pub struct Database<S: for<'a> StorageEngine<'a>> {
-    storage: S,
+pub trait Database {
+    type Transaction: Transaction;
+    fn begin(&'_ self) -> Self::Transaction;
 }
 
-pub trait Transaction<'a> {
-    fn get(&self, key: Key<'_>) -> Option<RV<'_>>;
-    fn set(&mut self, key: Key<'_>, value: RV<'_>) -> Result<(), ExecutionError>;
-    fn delete(&mut self, key: Key<'_>);
-    fn scan(&'a self, prefix: Key<'_>) -> impl Iterator<Item = StorageIteratorItem<'a>>;
+pub type TransactionActionResult<T> = Result<T, error::DatabaseError>;
+
+pub trait Transaction {
+    fn get(&self, key: Key<'_>) -> TransactionActionResult<Option<RV<'_>>>;
+    fn set(&mut self, key: Key<'_>, value: RV<'_>) -> TransactionActionResult<()>;
+    fn delete(&mut self, key: Key<'_>) -> TransactionActionResult<()>;
+    fn scan(&self, prefix: Key<'_>) -> TransactionActionResult<impl Iterator<Item = StorageIteratorItem<'_>> + '_>;
+}
+
+impl<T> From<std::sync::PoisonError<T>> for DatabaseError {
+    fn from(err: std::sync::PoisonError<T>) -> Self {
+        panic!("{err}")
+    }
 }
