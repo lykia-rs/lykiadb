6693e2735575a886bf51433e53cfac7c6c4fe013
V. Can Keklik <vcankeklik@gmail.com>
2026-05-25 14:01:33 +0300

    chore: Database structure

--- lykiadb-server/src/database/transaction.rs
+++
@@ -1,8 +0,0 @@
-use crate::{database::{Key, StorageIteratorItem}, execution::error::ExecutionError, value::RV};
-
-pub trait Transaction<'a> {
-    fn get(&self, key: Key<'_>) -> Option<RV<'_>>;
-    fn set(&mut self, key: Key<'_>, value: RV<'_>) -> Result<(), ExecutionError>;
-    fn delete(&mut self, key: Key<'_>);
-    fn scan(&'a self, prefix: Key<'_>) -> impl Iterator<Item = StorageIteratorItem<'a>>;
-}
